3645faa222474ec40bc8b53f8bdc4837f50f289f
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2025-04-26 17:57:20 +0300

    `Added console.log statement and modified message in handleMessage method`

--- src/bot/services/handle.service.ts
+++ src/bot/services/handle.service.ts
@@ -6,6 +6,8 @@ export class HandleService {
     async handeMessage() {
         const bot: TelegramBot = global.bot
         const chatId = process.env.ADMIN_ID
-        await bot.sendMessage(chatId, 'Hello world')
+        console.log(chatId)
+
+        await bot.sendMessage(chatId, 'hello')
     }
 }
