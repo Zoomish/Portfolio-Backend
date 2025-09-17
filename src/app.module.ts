4bc6df08ee50ed283db5f57e2df7cba601977e8c
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2025-09-17 19:08:48 +0300

    `Added AppController and AppController import to AppModule, and removed periodic fetch call from main.ts`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { AppController } from './app.controller'
 import { BotModule } from './bot/bot.module'
 
 @Module({
     imports: [ConfigModule.forRoot({ isGlobal: true }), BotModule],
+    controllers: [AppController],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,8 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     app.setGlobalPrefix('api')
     app.enableCors()
-    function aaa() {
-        setInterval(
-            async () => {
-                await fetch(
-                    'https://activityhelper-mln1.onrender.com/api/repos'
-                )
-            },
-            1000 * 60 * 10
-        )
-    }
 
     await app.listen(3000, async () => {
-        aaa()
         console.log(`Server started on port ${await app.getUrl()}`)
     })
 }
