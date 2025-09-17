fd3a2f5fc59d05adfbbf5bbbe8fa9d99615dbda0
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2025-09-17 19:33:59 +0300

    `feat: added cache-manager and cache-interceptor to the project`

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager'
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { APP_INTERCEPTOR } from '@nestjs/core'
 import { AppController } from './app.controller'
 import { BotModule } from './bot/bot.module'
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true }), BotModule],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        CacheModule.register({
+            ttl: 1000 * 60 * 60 * 24,
+            isGlobal: true,
+        }),
+        BotModule,
+    ],
     controllers: [AppController],
+    providers: [
+        {
+            provide: APP_INTERCEPTOR,
+            useClass: CacheInterceptor,
+        },
+    ],
 })
 export class AppModule {}
