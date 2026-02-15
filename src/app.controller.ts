0a5755688c33583ba9aab0b8a3588c0352bccfaa
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2026-02-15 16:15:05 +0300

    `Update AppController to use @Inject and async/await for cache management`

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common'
-import { CacheKey, CacheTTL } from '@nestjs/cache-manager'
+import { CACHE_MANAGER } from '@nestjs/cache-manager'
+import { Controller, Get, Inject } from '@nestjs/common'
+import { Cache } from 'cache-manager'
 
 @Controller('app')
 export class AppController {
+    constructor(@Inject(CACHE_MANAGER) private readonly cacheManager: Cache) {}
+
     @Get()
-    @CacheKey('app-hello')
-    @CacheTTL(900)
-    getHello(): string {
-        return 'Hello World!'
+    async getHello(): Promise<string> {
+        const cacheKey = 'app_hello'
+        const cachedValue = await this.cacheManager.get<string>(cacheKey)
+
+        if (cachedValue) {
+            return cachedValue
+        }
+
+        const newValue = `Hello World! ${new Date().toISOString()}`
+        await this.cacheManager.set(cacheKey, newValue, 900)
+        return newValue
     }
 }
