cb658d9a7c1001cae1135cfa183954aa4791d7bf
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2026-02-15 15:27:50 +0300

    `Added cache functionality to AppController's getHello method`

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get } from '@nestjs/common';
+import { CacheKey, CacheTTL } from '@nestjs/cache-manager';
 
 @Controller('app')
 export class AppController {
     @Get()
+    @CacheKey('app-hello')
+    @CacheTTL(600) 
     getHello(): string {
-        return 'Hello World!'
+        return 'Hello World!';
     }
 }
