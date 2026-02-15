5abe4ac92119bbfc6425c3af5e8172fb879c6e81
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2026-02-15 16:56:19 +0300

    `Removed RedisModule and replaced CacheModule registration with Redis-based caching`

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,21 +4,29 @@ import { ConfigModule, ConfigService } from '@nestjs/config'
 import { ScheduleModule } from '@nestjs/schedule'
 import { ThrottlerModule } from '@nestjs/throttler'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import * as redisStore from 'cache-manager-redis-store'
 import * as fs from 'fs'
 import { AppController } from './app.controller'
 import { BotModule } from './bot/bot.module'
+import { CASHED_DATA_TTL } from './const'
 import { GithubModule } from './github/github.module'
 import { LinkedinModule } from './linkedin/linkedin.module'
-import { RedisModule } from './redis.module'
 import { GetActiveService } from './tasks/getActiveService.service'
 import { UserModule } from './user/user.module'
 
 @Module({
     imports: [
         ConfigModule.forRoot({ isGlobal: true }),
-        CacheModule.register({
-            ttl: 1000 * 60 * 60 * 24,
-            isGlobal: true,
+        CacheModule.registerAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+                store: redisStore,
+                url: configService.get<string>('REDIS_URL'),
+                ttl: CASHED_DATA_TTL,
+                max: 100,
+                isGlobal: true,
+            }),
+            inject: [ConfigService],
         }),
         ScheduleModule.forRoot(),
         TypeOrmModule.forRootAsync({
@@ -62,7 +70,6 @@ import { UserModule } from './user/user.module'
             },
         ]),
         BotModule,
-        RedisModule,
         UserModule,
         GithubModule,
         LinkedinModule,
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
+import { ConsoleLogger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { ConsoleLogger } from '@nestjs/common'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
@@ -11,6 +11,11 @@ async function bootstrap() {
     })
     app.setGlobalPrefix('api')
     app.enableCors()
+    const redisClient = require('redis').createClient({
+        url: process.env.REDIS_URL,
+    })
+    redisClient.on('connect', () => console.log('Redis connected'))
+    redisClient.on('error', (err) => console.error('Redis error:', err))
 
     await app.listen(3000, async () => {
         console.log(`Server started on port ${await app.getUrl()}`)
